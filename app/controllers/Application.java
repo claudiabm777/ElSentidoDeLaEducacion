700f9bce17777195bde10ad598a0a3b82ba5550d
Claudia Bedoya <claudiabm777@gmail.com>
2015-09-27 03:36:32 -0500

    ggg

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -45,7 +45,7 @@ public class Application extends Controller {
         try {
             Email email = new Email();
             email.setSubject(subject);
-            email.setFrom("CopaGrader <sentido.educ@gmail.com>");
+            email.setFrom("sentido educacion<sentido.educ@gmail.com>");
             email.addTo("TO <"+subject+">");
             //email.addTo("<claudiabm777@gmail.com>");
             //System.out.println(email.getTo().toString());
